f87935fa3769dac616720aafcbb030e14b56b56e
Shanu <shanu.saini@netsolutions.com>
2019-08-30 09:23:48 +0530

    Adding Attribute
    Adding Attribute

--- lp2/src/components/counter.jsx
+++ lp2/src/components/counter.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+
 class Counter extends Component {
+    state = {
+        count: 0,
+    }
+    
     render() { 
-        return <h1>Hello World</h1>;
+        return (
+            <React.Fragment>
+                <span className="badge badge-primary m-2">{this.formateCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </React.Fragment>
+        );
+    }
+
+    formateCount(){
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
-export default Counter; 
+export default Counter;
