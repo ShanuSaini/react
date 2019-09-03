5ca5966d3e7f52f1acff94f7298183b844204637
Shanu <shanu.saini@netsolutions.com>
2019-09-03 09:44:21 +0530

    Multiple components in sync
    - Multiple components in sync
    - Moving state to app component

--- lp2/src/App.js
+++ lp2/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counters from './components/counters';
+import Navbar from './components/navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    Counters: [
+      {id: 1, value: 1 },
+      {id: 2, value: 0 },
+      {id: 3, value: 0 },
+      {id: 4, value: 0 }
+    ]
+  }
+
+  handleDelete = (counterId) => {
+    const counters = this.state.Counters.filter(c => c.id !== counterId);
+    this.setState({
+      Counters: counters
+    });
+  }
+
+  handleReset = () => {
+    const counters = this.state.Counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({
+      Counters: counters
+    });
+  }
+
+  handleIncrement = counter => {
+    const counters = [...this.state.Counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value++;
+    this.setState({
+      Counters: counters
+    });
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <Navbar totalCounters={this.state.Counters.filter(c => c.value > 0).length}/>
+        <main className="container">
+          <Counters 
+            Counters={this.state.Counters} 
+            onReset={this.handleReset} 
+            onIncrement={this.handleIncrement} 
+            onDelete={this.handleDelete} 
+          />
+        </main>
+      </React.Fragment>
+    );
+  };
+
 }
 
 export default App;
--- lp2/src/components/counters.jsx
+++ lp2/src/components/counters.jsx
@@ -2,49 +2,14 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = { 
-        Counters: [
-            {id: 1, value: 1 },
-            {id: 2, value: 0 },
-            {id: 3, value: 0 },
-            {id: 4, value: 0 }
-        ]
-    }
-
-    handleDelete = (counterId) => {
-        const counters = this.state.Counters.filter(c => c.id !== counterId);
-        this.setState({
-            Counters: counters
-        });
-    }
-
-    handleReset = () => {
-        const counters = this.state.Counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-        this.setState({
-            Counters: counters
-        });
-    }
-
-    handleIncrement = counter => {
-        const counters = [...this.state.Counters];
-        const index = counters.indexOf(counter);
-        counters[index] = {...counter};
-        counters[index].value++;
-        this.setState({
-            Counters: counters
-        });
-    }
 
     render() { 
-        return ( 
+        return (
             <React.Fragment>
-            <button className="btn btn-primary btn-sm m-2" onClick={this.handleReset}>Reset</button>
-                { this.state.Counters.map(
+            <button className="btn btn-primary btn-sm m-2" onClick={this.props.onReset}>Reset</button>
+                { this.props.Counters.map(
                     counter => 
-                        <Counter key={counter.id} onDelete={this.handleDelete} onIncrement={this.handleIncrement} counter={counter}></Counter>
+                        <Counter key={counter.id} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} counter={counter}></Counter>
                     ) 
                 }
             </React.Fragment> 
