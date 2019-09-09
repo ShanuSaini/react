9fb117826fcf625cefe1e24f8a9b23d0549e0f3c
ShanuSaini <shanusaini434@gmail.com>
2019-09-09 12:56:12 -0400

    Creating seprate table component
    Creating seprate table component

--- lp3/src/components/movies.jsx
+++ lp3/src/components/movies.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { getMovies  } from '../services/fakeMovieService';
 import { getGenres } from '../services/fakeGenreService.js';
-import Like from './common/like';
+import MoviesTable from './moviesTable';
 import Pagination from './common/pagination';
 import { paginate } from '../utills/paginate';
 import ListGroup from './common/listGroup';
@@ -24,7 +24,6 @@ class Movies extends Component {
 
     constructor(){
         super();
-        this.getRows = this.getRows.bind(this);
         this.deleteMovieFun = this.deleteMovieFun.bind(this);
     }
 
@@ -46,29 +45,6 @@ class Movies extends Component {
     handlePageChange = (page) => {
         // console.log('handlePageChange = ', page);
         this.setState({ currentPage: page });
-    }
-
-    getRows(movies) {
-        return movies.map( 
-            movie => {
-                return (
-                    <tr key={movie['_id']}>
-                        <td>{movie['title']}</td>
-                        <td>{movie['genre']['name']}</td>
-                        <td>{movie['numberInStock']}</td>
-                        <td>{movie['dailyRentalRate']}</td>
-                        <td>
-                            <Like liked={movie['liked']} onClick={ () => this.handleLike(movie) }/>
-                        </td>
-                        <td>
-                            <button onClick={ () => this.deleteMovieFun(movie)} type="button" className="btn btn-danger">
-                                Delete
-                            </button>
-                        </td>
-                    </tr>
-                );
-            }
-        );
     }
 
     handleGenreSelect = genre => {
@@ -101,21 +77,11 @@ class Movies extends Component {
                 <div className="col">
                     <p>Showing {filtered.length} movies in the database.</p>
                     <div className="container">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Genre</th>
-                                    <th scope="col">Stock</th>
-                                    <th scope="col">Rate</th>
-                                    <th scope="col"></th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                { this.getRows(movies) }
-                            </tbody>
-                        </table>
+                        <MoviesTable 
+                            movies={movies}
+                            onLike={this.handleLike}
+                            onDelete={this.deleteMovieFun}
+                        />
                         <Pagination 
                             itemsCount={filtered.length} 
                             pageSize={pageSize} 
