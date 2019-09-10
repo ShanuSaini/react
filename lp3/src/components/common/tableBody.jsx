c11c354a31338bd7d1d7e87980346f4185e7988e
ShanuSaini <shanusaini434@gmail.com>
2019-09-09 22:27:42 -0400

    Unique keys
    Unique keys

--- lp3/src/components/common/tableBody.jsx
+++ lp3/src/components/common/tableBody.jsx
@@ -9,18 +9,22 @@ class TableBody extends Component {
     return _.get(item, column.path)
   }
 
+  createKey = (item, column) => {
+    return item._id+(column.path || column.key);
+  }
+
   render() { 
 
     const { data, columns } = this.props;
-    console.log(columns);
+    // console.log(columns);
 
     return ( 
       <tbody>
         {data.map( 
           item => (
-            <tr>
+            <tr key={item._id}>
               { 
-                columns.map(column => <td>{ this.renderCell(item, column) }</td>) 
+                columns.map(column => <td key={this.createKey(item, column)}>{ this.renderCell(item, column) }</td>) 
               }
             </tr>
           )
