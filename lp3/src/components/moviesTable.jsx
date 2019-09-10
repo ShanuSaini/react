3e82806f7d5cacf4bd914b3c676b851cd38fdfc3
ShanuSaini <shanusaini434@gmail.com>
2019-09-10 12:51:56 -0400

    Extracting Table
    Extracting Table

--- lp3/src/components/moviesTable.jsx
+++ lp3/src/components/moviesTable.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
+
 
 class MoviesTable extends Component {
 
@@ -33,17 +33,12 @@ class MoviesTable extends Component {
     const { movies, onSort, sortColumn } = this.props;
     
     return(
-      <table className="table">
-        <TableHeader 
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody 
-          data={movies}
-          columns={this.columns}
-        />
-      </table>
+      <Table 
+        columns={this.columns} 
+        data={movies} 
+        sortColumn={sortColumn} 
+        onSort={onSort}
+      />
     );
     
   }
