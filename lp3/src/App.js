93575732ec48175032776a89e61f5a709dac6762
ShanuSaini <shanusaini434@gmail.com>
2019-09-23 13:52:23 -0400

    Adding Save button to redirect user
    Adding Save button to redirect user

--- lp3/src/App.js
+++ lp3/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 
+import MovieForm from "./components/movieForm";
 import Movies from "./components/movies";
 import Customers from './components/customers';
 import Rentals from './components/rentals';
@@ -14,6 +15,7 @@ function App() {
       <NavBar />
       <main className="container pt-5">
         <Switch>
+          <Route path="/movies/:id" component={MovieForm}></Route>
           <Route path="/movies" component={Movies}></Route>
           <Route path="/customers" component={Customers}></Route>
           <Route path="/rentals" component={Rentals}></Route>
